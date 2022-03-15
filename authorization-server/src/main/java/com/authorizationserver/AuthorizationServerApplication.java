23558d514bdcd3774153af7b490a5ac14458d34e
gilszon <gilszon@gmail.com>
2022-03-14 21:37:19 -0300

    Authorization Server

---
+++ authorization-server/src/main/java/com/authorizationserver/AuthorizationServerApplication.java
@@ -0,0 +1,28 @@
+package com.authorizationserver;
+
+import java.security.Principal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@EnableResourceServer
+@SpringBootApplication
+public class AuthorizationServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthorizationServerApplication.class, args);
+	}
+	
+	//This method will be used to check if the user has a valid token to access the resource
+	@RequestMapping("/validateUser")
+	public Principal user(Principal user) {
+		return user;
+	}
+
+}
